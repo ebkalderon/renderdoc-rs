ec9b71889cf0c731aa3c76dcb01bada7db4d0735
Eyal Kalderon <ebkalderon@gmail.com>
2019-05-19 05:13:19 +0800

    Deny missing debug implementations

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 //! [api]: https://renderdoc.org/docs/in_application_api.html
 
 #![deny(missing_docs)]
+#![deny(missing_debug_implementations)]
 
 #[cfg(any(target_os = "macos", target_os = "ios"))]
 compile_error!("RenderDoc does not support this platform.");
