0d7e4ec43883222db5a0adf2bf5d58c58254b67f
Eyal Kalderon <ebkalderon@gmail.com>
2023-01-28 00:45:33 -0500

    Start from scratch, rewrite version representation
    The whole type-safe runtime downgrading API with `.into()` is honestly
    an unnecessary gimmick and should be removed. Pretty much no one would
    ever want nor need to downgrade to a different version of the RenderDoc
    API at runtime, much less do it in a type-safe way. The types, traits,
    and macro necessary for this feature is hardly worth the maintenance
    burden. Additionally, the way the current codebase abuses `Deref` and
    `DerefMut` with `std::mem::transmute` (!!) to mimic a kind of
    inheritance pattern is a big and unnecessary red flag.
    
    With all that said, however, the core concept of providing a type-safe
    interface to RenderDoc guaranteed using traits and generics is still
    valid, and perhaps exposing a fallible way to _upgrade_ your API rather
    than an infallible way to downgrade would be more useful.
    
    As such, this commit throws out all the existing code in the `renderdoc`
    crate with the intention of rewriting it to be more ergonomic and user
    friendly. The reimagined version model relies on a `Minimum<V: Version>`
    trait to constrain API version numbers instead of `Deref{,Mut}`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,129 +1,44 @@
-//! Rust bindings to [RenderDoc], a popular graphics debugger.
-//!
-//! [RenderDoc]: https://renderdoc.org/
-//!
-//! RenderDoc is a free and open source debugger for real-time graphics providing quick and easy
-//! frame captures and detailed introspection of any application using [Vulkan], [Direct3D 11],
-//! [Direct3D 12], [OpenGL], and [OpenGL ES].
-//!
-//! [Vulkan]: https://www.vulkan.org/
-//! [Direct3D 11]: https://learn.microsoft.com/en-us/windows/win32/direct3d11/atoc-dx-graphics-direct3d-11
-//! [Direct3D 12]: https://learn.microsoft.com/en-us/windows/win32/direct3d12/direct3d-12-graphics
-//! [OpenGL]: https://www.khronos.org/opengl/
-//! [OpenGL ES]: https://www.khronos.org/opengles/
-//!
-//! These bindings require that RenderDoc be installed on the target machine, with either
-//! `renderdoc.dll` or `librenderdoc.so` visible from your `$PATH`.
-//!
-//! For more details on how to use this API to integrate your game or renderer with the RenderDoc
-//! profiler, consult the upstream [in-application API][in-app] documentation.
-//!
-//! [in-app]: https://renderdoc.org/docs/in_application_api.html
-
-#![deny(missing_docs)]
-#![deny(missing_debug_implementations)]
-
 #[cfg(any(target_os = "macos", target_os = "ios"))]
 compile_error!("RenderDoc does not support this platform.");
 
+use std::marker::PhantomData;
+
 pub use self::error::Error;
-pub use self::handles::{DevicePointer, WindowHandle};
-pub use self::renderdoc::RenderDoc;
-pub use self::settings::{CaptureOption, InputButton, OverlayBits};
 pub use self::version::{
-    Entry, HasPrevious, Version, V100, V110, V111, V112, V120, V130, V140, V141,
+    Minimum, RawRenderDoc, Version, V100, V101, V102, V110, V111, V112, V120, V130, V140, V141,
+    V142, V150, V160,
 };
-
-use std::os::raw::c_ulonglong;
-
-#[cfg(windows)]
-use winapi::shared::guiddef::GUID;
 
 mod error;
-mod handles;
-mod renderdoc;
-mod settings;
 mod version;
 
-/// Magic value used for when applications pass a path where shader debug information can be found
-/// to match up with a stripped shader.
-///
-/// Windows GUID representation intended for consumption by D3D.
-#[cfg(windows)]
-pub const SHADER_MAGIC_DEBUG_VALUE_STRUCT: GUID = GUID {
-    Data1: 0xeab25520,
-    Data2: 0x6670,
-    Data3: 0x4865,
-    Data4: [0x84, 0x29, 0x6c, 0x8, 0x51, 0x54, 0x00, 0xff],
-};
+pub struct RenderDoc<V = V160> {
+    api: *mut RawRenderDoc,
+    _version: PhantomData<V>,
+}
 
-/// Magic value used for when applications pass a path where shader debug information can be found
-/// to match up with a stripped shader.
-///
-/// Raw byte array representation (assuming x86 endianness).
-pub const SHADER_MAGIC_DEBUG_VALUE_BYTE_ARRAY: &[u8] = &[
-    0x20, 0x55, 0xb2, 0xea, 0x70, 0x66, 0x65, 0x48, 0x84, 0x29, 0x6c, 0x8, 0x51, 0x54, 0x00, 0xff,
-];
-
-/// Magic value used for when applications pass a path where shader debug information can be found
-/// to match up with a stripped shader.
-///
-/// Truncated version when only a `uint64_t` is available (e.g. Vulkan tags).
-pub const SHADER_MAGIC_DEBUG_VALUE_TRUNCATED: c_ulonglong = 0x0485_6670_eab2_5520;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn get_set_capture_option_f32() {
-        let mut rd: RenderDoc<V110> = RenderDoc::new().expect("Failed to init");
-
-        let delay = rd.get_capture_option_f32(CaptureOption::DelayForDebugger);
-        assert_eq!(delay, 0.0f32);
-
-        rd.set_capture_option_f32(CaptureOption::DelayForDebugger, 2.5f32);
-        let delay = rd.get_capture_option_f32(CaptureOption::DelayForDebugger);
-        assert_eq!(delay, 2.0f32);
+impl<V: Version> RenderDoc<V> {
+    /// Initializes a new instance of the RenderDoc API.
+    ///
+    /// Note that RenderDoc will usually provide a higher API version than the one requested by
+    /// the user if it is backwards compatible.
+    pub fn new() -> Result<Self, Error> {
+        Ok(RenderDoc {
+            api: V::load()?,
+            _version: PhantomData,
+        })
     }
 
-    #[test]
-    fn get_set_capture_option_u32() {
-        let rd: RenderDoc<V110> = RenderDoc::new().expect("Failed to init");
-
-        let vsync = rd.get_capture_option_u32(CaptureOption::AllowVSync);
-        assert_eq!(vsync, 1u32);
-
-        let is_full = rd.get_capture_option_u32(CaptureOption::AllowFullscreen);
-        assert_eq!(is_full, 1u32);
-
-        let api_val_mode = rd.get_capture_option_u32(CaptureOption::ApiValidation);
-        let debug_mode = rd.get_capture_option_u32(CaptureOption::ApiValidation);
-        assert_eq!(api_val_mode, 0u32);
-        assert_eq!(api_val_mode, debug_mode);
-
-        let cc = rd.get_capture_option_u32(CaptureOption::CaptureCallstacks);
-        assert_eq!(cc, 0u32);
-
-        let cc_draw = rd.get_capture_option_u32(CaptureOption::CaptureCallstacksOnlyDraws);
-        assert_eq!(cc_draw, 0u32);
-
-        let ver_map = rd.get_capture_option_u32(CaptureOption::VerifyMapWrites);
-        assert_eq!(ver_map, 0u32);
-
-        let hook_in = rd.get_capture_option_u32(CaptureOption::HookIntoChildren);
-        assert_eq!(hook_in, 0u32);
-
-        let ref_all = rd.get_capture_option_u32(CaptureOption::RefAllResources);
-        assert_eq!(ref_all, 0u32);
-
-        let intls = rd.get_capture_option_u32(CaptureOption::SaveAllInitials);
-        assert_eq!(intls, 1u32);
-
-        let cmds = rd.get_capture_option_u32(CaptureOption::CaptureAllCmdLists);
-        assert_eq!(cmds, 0u32);
-
-        let is_muted = rd.get_capture_option_u32(CaptureOption::DebugOutputMute);
-        assert_eq!(is_muted, 1u32);
+    /// Returns the underlying API entry point struct.
+    ///
+    /// # Safety
+    ///
+    /// Directly accessing this function table discards any and all safety features of this library.
+    pub unsafe fn raw_api(&self) -> *mut RawRenderDoc {
+        self.api
     }
 }
+
+impl<V: Minimum<V100>> RenderDoc<V> {}
+
+unsafe impl<V> Send for RenderDoc<V> {}
--- src/version.rs
+++ src/version.rs
@@ -1,57 +1,26 @@
-//! Entry points for the RenderDoc API.
-
-use std::ffi::c_void;
+use std::ffi::{c_uint, c_void};
 use std::ptr;
 
 use libloading::{Library, Symbol};
 use once_cell::sync::OnceCell;
-use renderdoc_sys::RENDERDOC_API_1_4_1;
+use renderdoc_sys::RENDERDOC_Version;
 
-use crate::error::Error;
+use crate::Error;
 
-/// Entry point for the RenderDoc API.
-pub type Entry = RENDERDOC_API_1_4_1;
+pub type RawRenderDoc = renderdoc_sys::RENDERDOC_API_1_6_0;
 
-/// Available versions of the RenderDoc API.
-#[repr(u32)]
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
-pub enum VersionCode {
-    /// Version 1.0.0.
-    V100 = 10000,
-    /// Version 1.0.1.
-    V101 = 10001,
-    /// Version 1.0.2.
-    V102 = 10002,
-    /// Version 1.1.0.
-    V110 = 10100,
-    /// Version 1.1.1.
-    V111 = 10101,
-    /// Version 1.1.2.
-    V112 = 10102,
-    /// Version 1.2.0.
-    V120 = 10200,
-    /// Version 1.3.0.
-    V130 = 10300,
-    /// Version 1.4.0.
-    V140 = 10400,
-    /// Version 1.4.1.
-    V141 = 10401,
-}
-
-/// Entry point into the RenderDoc API.
 pub trait Version {
-    /// Minimum compatible version number.
-    const VERSION: VersionCode;
+    const VERSION: RENDERDOC_Version;
 
     /// Initializes a new instance of the RenderDoc API.
     ///
     /// # Safety
     ///
     /// This function is not thread-safe and should not be called on multiple threads at once.
-    fn load() -> Result<*mut Entry, Error> {
+    fn load() -> Result<*mut RawRenderDoc, Error> {
         static LIBRARY: OnceCell<Library> = OnceCell::new();
 
-        type GetApiFn = unsafe extern "C" fn(ver: u32, out: *mut *mut c_void) -> i32;
+        type GetApiFn = unsafe extern "C" fn(ver: c_uint, out: *mut *mut c_void) -> i32;
 
         #[cfg(windows)]
         let lib_path = "renderdoc.dll";
@@ -90,108 +59,80 @@ pub trait Version {
                 lib.get(b"RENDERDOC_GetAPI\0").map_err(Error::symbol)?;
 
             let mut obj = ptr::null_mut();
-            match get_api(Self::VERSION as u32, &mut obj) {
-                1 => Ok(obj as *mut Entry),
+            match get_api(Self::VERSION as c_uint, &mut obj) {
+                1 => Ok(obj as *mut RawRenderDoc),
                 _ => Err(Error::no_compatible_api()),
             }
         }
     }
 }
 
-/// Trait for statically enforcing backwards compatibility.
-pub trait HasPrevious: Version {
-    /// API version which immediately precedes this one.
-    type Previous: Version;
-}
+pub trait Minimum<V: Version> {}
 
-/// Requests a minimum version number of 1.0.0.
-#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
+pub trait HasShutdown {}
+
+pub trait HasRemoveHooks {}
+
 pub enum V100 {}
-
-impl Version for V100 {
-    const VERSION: VersionCode = VersionCode::V100;
-}
-
-/// Requests a minimum version number of 1.1.0.
-#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
+pub enum V101 {}
+pub enum V102 {}
 pub enum V110 {}
-
-impl Version for V110 {
-    const VERSION: VersionCode = VersionCode::V110;
-}
-
-impl HasPrevious for V110 {
-    type Previous = V100;
-}
-
-/// Requests a minimum version number of 1.1.1.
-#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 pub enum V111 {}
-
-impl Version for V111 {
-    const VERSION: VersionCode = VersionCode::V111;
-}
-
-impl HasPrevious for V111 {
-    type Previous = V110;
-}
-
-/// Requests a minimum version number of 1.1.2.
-#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 pub enum V112 {}
-
-impl Version for V112 {
-    const VERSION: VersionCode = VersionCode::V112;
-}
-
-impl HasPrevious for V112 {
-    type Previous = V111;
-}
-
-/// Requests a minimum version number of 1.2.0.
-#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 pub enum V120 {}
-
-impl Version for V120 {
-    const VERSION: VersionCode = VersionCode::V120;
-}
-
-impl HasPrevious for V120 {
-    type Previous = V112;
-}
-
-/// Requests a minimum version number of 1.3.0.
-#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 pub enum V130 {}
-
-impl Version for V130 {
-    const VERSION: VersionCode = VersionCode::V130;
-}
-
-impl HasPrevious for V130 {
-    type Previous = V120;
-}
-
-/// Requests a minimum version number of 1.4.0.
-#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 pub enum V140 {}
-
-impl Version for V140 {
-    const VERSION: VersionCode = VersionCode::V140;
-}
-
-impl HasPrevious for V140 {
-    type Previous = V130;
-}
-
-/// Requests a minimum version number of 1.4.1.
-#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 pub enum V141 {}
+pub enum V142 {}
+pub enum V150 {}
+pub enum V160 {}
 
-impl Version for V141 {
-    const VERSION: VersionCode = VersionCode::V141;
+macro_rules! define_versions {
+    ($($name:ident => $version_code:ident $( ( $($mixin:ident),* ) )?),+) => {
+        $(define_versions!(@version $name $version_code $( $($mixin)+ )?);)+
+
+        define_versions!(@minimum $($name)+);
+    };
+
+    (@version $name:ident $version_code:ident $($mixin:ident)*) => {
+        impl Version for $name {
+            const VERSION: RENDERDOC_Version = renderdoc_sys::$version_code;
+        }
+
+        $(impl $mixin for $name {})*
+    };
+
+    (@minimum $first:ident $($rest:ident)*) => {
+        impl<V: Version> Minimum<$first> for V {}
+
+        define_versions!(@minimum_rec $($rest)*);
+    };
+
+    (@minimum_rec $first:ident $second:ident $($rest:ident)*) => {
+        impl Minimum<$first> for $first {}
+
+        impl<V: Minimum<$second>> Minimum<$first> for V {}
+
+        define_versions!(@minimum_rec $second $($rest)*);
+    };
+
+    (@minimum_rec $single:ident) => {
+        impl Minimum<$single> for $single {}
+    };
 }
 
-impl HasPrevious for V141 {
-    type Previous = V140;
+define_versions! {
+    V100 => eRENDERDOC_API_Version_1_0_0 (HasShutdown),
+    V101 => eRENDERDOC_API_Version_1_0_1 (HasShutdown),
+    V102 => eRENDERDOC_API_Version_1_0_2 (HasShutdown),
+    V110 => eRENDERDOC_API_Version_1_1_0 (HasShutdown),
+    V111 => eRENDERDOC_API_Version_1_1_1 (HasShutdown),
+    V112 => eRENDERDOC_API_Version_1_1_2 (HasShutdown),
+    V120 => eRENDERDOC_API_Version_1_2_0 (HasShutdown),
+    V130 => eRENDERDOC_API_Version_1_3_0 (HasShutdown),
+    V140 => eRENDERDOC_API_Version_1_4_0 (HasShutdown),
+    V141 => eRENDERDOC_API_Version_1_4_1 (HasRemoveHooks),
+    V142 => eRENDERDOC_API_Version_1_4_2 (HasRemoveHooks),
+    V150 => eRENDERDOC_API_Version_1_5_0 (HasRemoveHooks),
+    V160 => eRENDERDOC_API_Version_1_6_0 (HasRemoveHooks)
 }
